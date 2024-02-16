789057f54026f3a40df4b1246baf51053eaf65f4
Giada Unger <giada.unger02@gmail.com>
2024-02-16 14:27:40 +0100

    Did some endpoints

---
+++ fast_api_intro/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+
+data = {
+  "users": [
+    {
+      "id": 1,
+      "name": "John Doe",
+      "email": "john.doe@example.com",
+      "is_active": True
+    },
+    {
+      "id": 2,
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com",
+      "is_active": False
+    }
+  ]
+}
+
+app = FastAPI()
+
+@app.get("/", tags=["status"])
+def get_status():
+    return {"message": "OK"}
+
+@app.get("/users", tags=["users"])
+def get_users():
+    return data["users"]
+
+@app.get("/users/{id}", tags=["users"])
+def get_user_details(id: int):
+    return {}
+
+@app.get("/users/list", tags=["users"])
+def get_users(limit: int):
+    if limit is not None:
+        return data["users"][:limit]
+    return data["users"]
