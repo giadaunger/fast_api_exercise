463bfab1e6cdbd40c3ccf1ff319362a1f202a56f
Giada Unger <giada.unger02@gmail.com>
2024-02-17 11:55:46 +0100

    Added some more data, endpoints and validation

--- fast_api_intro/main.py
+++ fast_api_intro/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from schemas import UserSchema
+from fastapi import FastAPI, HTTPException
+from schemas import UserSchema, UserPostsSchema
 
 data = {
   "users": [
@@ -7,13 +7,34 @@ data = {
       "id": 1,
       "name": "John Doe",
       "email": "john.doe@example.com",
-      "is_active": True
+      "is_active": True,
+      "profile": {"age": 30, "bio": "Blablabla"},
+      "posts": [
+        {
+          "id": 1,
+          "title": "John's First Post",
+          "content": "This is the content of John's first post."
+        },
+        {
+          "id": 2,
+          "title": "John's Second Post",
+          "content": "This is the content of John's second post."
+        }
+      ]
     },
     {
       "id": 2,
       "name": "Jane Doe",
-      "email": "jane.doe@example.com",
-      "is_active": False
+      "email": "jane.doe@example.com", # fix
+      "is_active": False,
+      "profile": {"age": 35, "bio": "sadsadsadsaBlablabla"},# fix
+      "posts": [
+        {
+          "id": 1,
+          "title": "Jane's First Post",
+          "content": "This is the content of Jane's first post."
+        }
+      ] #fix
     }
   ]
 }
@@ -22,23 +43,32 @@ app = FastAPI()
 
 @app.get("/", tags=["status"])
 def get_status():
-    return {"message": "OK"}
+    return {"message": "OK!!!!"}
 
 @app.get("/users", tags=["users"])
 def get_users():
     return data["users"]
 
-@app.get("/users/{id}", tags=["users"])
-def get_user_details(id: int):
-    return {}
-
 @app.get("/users/list", tags=["users"])
-def get_users(limit: int):
+def get_users_limit(limit: int | None = None):
     if limit is not None:
         return data["users"][:limit]
     return data["users"]
 
+@app.get("/users/{id}", tags=["users"])
+def get_user_detail(id: int, name: str):
+    print(name)
+    for user in data["users"]:
+        if user["id"] == id:
+            return user
+    raise HTTPException(status_code=404, detail="User not found")
+    
 @app.post("/users", tags=["users"])
 def add_user(user: UserSchema):
+    data["users"].append(user.model_dump())
+    return user
+
+@app.post("/posts/users", tags=["users"])
+def add_user(user: UserPostsSchema):
     data["users"].append(user.model_dump())
     return user
--- fast_api_intro/schemas.py
+++ fast_api_intro/schemas.py
@@ -1,7 +1,31 @@
 from pydantic import BaseModel, Field, field_validator
+    
+class ProfileSchema(BaseModel):
+    bio: str = Field(max_length=2000)
+    age: int = Field(gt=0, lt=500)
+    
+class PostSchema(BaseModel):
+    id: int = Field(gt=0)
+    title: str
+    content: str
 
 class UserSchema(BaseModel):
-    id: int = Field(gt=0, lte=100000000000000)
+    id: int = Field(gt=0, lt=10000000000)
     name: str = Field(min_length=1)
-    email: str | None = Field(default=None, max_length=20, min_length=5)
     is_active: bool
+    email: str | None = Field(default=None, max_length=20, min_length=5)
+
+class UserPostsSchema(BaseModel):
+    id: int = Field(gt=0, lt=10000000000)
+    name: str = Field(min_length=1)
+    is_active: bool
+    email: str | None = Field(default=None, max_length=20, min_length=5)
+    profile: ProfileSchema
+    posts: list[PostSchema]
+    
+    @field_validator("name")
+    def validate_name(cls, name: str) -> str:
+        print(name)
+        if name == "crap":
+            raise ValueError("Error: Bad language")
+        return name
